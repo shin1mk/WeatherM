25f57c5af7af93e3acc06379cfd82a80add0a71a
MK1 <shin.m.dp@gmail.com>
2023-07-31 00:28:19 +0300

    added fonts

---
+++ WeatherM/Resources/Fonts/CustomFonts.swift
@@ -0,0 +1,18 @@
+//
+//  CustomFonts.swift
+//  WeatherM
+//
+//  Created by SHIN MIKHAIL on 30.07.2023.
+//
+
+import Foundation
+import UIKit
+
+extension UIFont {
+    static func ubuntuLight(ofSize size: CGFloat) -> UIFont? {
+        return UIFont(name: "Ubuntu-Light", size: size)
+    }
+    static func ubuntuRegular(ofSize size: CGFloat) -> UIFont? {
+        return UIFont(name: "Ubuntu-Regular", size: size)
+    }
+}
