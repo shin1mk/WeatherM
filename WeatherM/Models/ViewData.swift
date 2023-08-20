fbae0b7186b4a36a6d8b5d90a5987728dc05d161
MK1 <shin.m.dp@gmail.com>
2023-08-20 13:58:14 +0300

    add cityData 2.1

---
+++ WeatherM/Models/ViewData.swift
@@ -0,0 +1,14 @@
+//
+//  ViewData.swift
+//  WeatherM
+//
+//  Created by SHIN MIKHAIL on 20.08.2023.
+//
+
+import Foundation
+
+struct ViewData {
+    let temperature: String
+    let city: String
+    let weather: String
+}
